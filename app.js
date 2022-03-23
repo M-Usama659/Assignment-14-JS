209e2d4c520963b94bb6a4b0193f865b64d242fc
M-Usama659 <musama659@gmail.com>
2022-03-24 01:16:39 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//  ------Chapter No. 1------
+
+// Question 1
+// alert("Hello User")
+
+// Question 2
+// alert("Error! Please enter avalid Password")
+
+// Question 3
+// alert("Welcome to JS land...\nHappy Coding!")
+
+// Question 4
+// alert("Welcome to JS land...")
+// alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+
+// Question 5
+// alert("Hello... I Can JS through my web browser's Console")
+
+// Question 6
